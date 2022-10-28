29acdc09563d84256d174108ade740ab6294d2e4
Beenamol <beena.ashish@bridgeconn.com>
2022-10-28 16:57:37 +0530

    Modified the file with removing quotes

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 </Head>
       <div>
       <h1 className = {styles.title}>Homepage</h1>
-      <p className = {styles.text}>Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites. React documentation mentions Next.js among "Recommended Toolchains" advising it to developers as a solution when "Building a server-rendered website with Node.js"</p>
+      <p className = {styles.text}>Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites. React documentation mentions Next.js among Recommended Toolchains advising it to developers as a solution when Building a server-rendered website with Node.js</p>
       <p className = {styles.text}> Where traditional React apps can only render their content in the client-side browser, Next.js extends this functionality to include applications rendered on the server-side.</p>
       <Link href="/beena/" className = {styles.btn}>
         See Ninja Listing
