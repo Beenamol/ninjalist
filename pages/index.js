a579cd75d20f8b7edc89142f45244f11e27ead4f
Beenamol <beena.ashish@bridgeconn.com>
2022-10-28 17:05:01 +0530

    changed component name

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Footer from '../comps/Footer'
-import Navbar from '../comps/navbar'
+import Navbar from '../comps/Navbar'
 import Link from 'next/link'
 
 export default function Home() {
